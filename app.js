5af58a2bc721fe6a2dc4e89fcb93114c73068230
Siddharth Jain <jains7186@gmail.com>
2017-10-09 18:18:29 +0530

    nodemodules1

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var io = require('socket.io')(express);
+var mysql = require('mysql');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
