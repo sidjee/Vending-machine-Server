38cfbef29819cfffcefde9fe545baa94c84b6951
Siddharth Jain <jains7186@gmail.com>
2017-10-15 01:32:47 +0530

    token based authentication and verification

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var cors = require('cors');
 var mysqlpass = require('../mypass.js');
 var index = require('./routes/index');
 var users = require('./routes/users');
+var quotes = require('./routes/quotes');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use(users);
+app.use(quotes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
